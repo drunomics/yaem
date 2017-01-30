17177577badd21750e312644463b136f63006b96
Mathias Mueller <mathias.mueller@drunomics.com>
2017-01-30 16:19:07 +0100

    Added yaem config form descriptions

--- src/Form/EmbedSettingsForm.php
+++ src/Form/EmbedSettingsForm.php
@@ -43,18 +43,21 @@ class EmbedSettingsForm extends ConfigFormBase {
       '#type' => 'textfield',
       '#title' => $this->t('Oembed: Embedly key'),
       '#default_value' => $config->get(self::OEMBED_EMBEDLY_KEY),
+      '#description' => t('If it\'s defined, use embed.ly api as fallback oembed provider.'),
     );
 
     $form[self::OEMBED_IFRAMELY_KEY] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Oembed: Iframely key'),
       '#default_value' => $config->get(self::OEMBED_IFRAMELY_KEY),
+      '#description' => t('If it\'s defined, use iframe.ly api as fallback oembed provider.'),
     );
 
     $form[self::GOOGLE_KEY] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Google: Key'),
       '#default_value' => $config->get(self::GOOGLE_KEY),
+      '#description' => t('Used only for google maps to generate the embed code.'),
     );
 
     return parent::buildForm($form, $form_state);
